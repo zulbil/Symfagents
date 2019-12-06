40367d0f12b701bac89e3b6dc8f42a37b2aa9559
zulbil <alexkhang25@yahoo.fr>
2019-12-06 08:15:00 +0100

    Adding redirection on login Page when a user is connected

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,20 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
 
 class SecurityController extends AbstractController
 {
+    private $security;
+    private $urlGenerator;
+
+    public function __construct(Security $security, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->security     =   $security;
+        $this->urlGenerator =   $urlGenerator;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        $isAuthenticate     = $this->security->isGranted('IS_AUTHENTICATED_FULLY');
+        if ($isAuthenticate) {
+            $hasAccess = $this->security->isGranted('ROLE_ADMIN');
+            if ($hasAccess) {
+                return new RedirectResponse($this->urlGenerator->generate('projets'));
+            }
+            return new RedirectResponse($this->urlGenerator->generate('project_user'));
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
