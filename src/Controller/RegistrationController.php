df561090c08d92da43671cd47680c4de645bcf68
zulbil <alexkhang25@yahoo.fr>
2019-12-11 17:37:15 +0100

    Change user password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -26,7 +28,11 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+//use Symfony\Component\Validator\Constraints\File;
+
 
 class RegistrationController extends AbstractController
 {
@@ -157,13 +163,14 @@ class RegistrationController extends AbstractController
             "nom"           => $user->getNom(),
             "postnom"       => $user->getPostnom(),
             "fonction"      => $user->getfonction(),
-            "email"         => $user->getEmail()
+            "email"         => $user->getEmail(),
+            //"photo"         => $user->setPhoto(new File($this->getParameter('uploads_directory').'/'.$user->getPhoto()))
         );
 
         $formProfil = $this->createFormBuilder($userProfil)
                            ->add('file', FileType::class, [
                                'constraints' => [
-                                   new File([
+                                   new \Symfony\Component\Validator\Constraints\File([
                                        'maxSize' => '5024k',
                                        'mimeTypes' => [
                                            'image/jpg',
@@ -216,6 +223,87 @@ class RegistrationController extends AbstractController
         $data['form'] = $formProfil->createView();
         return $this->render('app/profile.html.twig', $data );
     }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/user/account", name="user_account")
+     */
+    public function userAccountSettings(Request $request)
+    {
+        $data = array();
+        $data['page'] = "Paramètres du compte";
+        $data['user'] = $this->entityManager->getRepository(User::class)->find($this->getUser()->getId());
+
+
+        return $this->render('app/account-settings.html.twig', $data );
+    }
+
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/user/password", name="user_password")
+     */
+    public function userPasswordSettings(Request $request, LoginFormAuthenticator $authenticator, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $data = array();
+        $data['page'] = "Changer le mot de passe";
+        $data['user'] = $this->entityManager->getRepository(User::class)->find($this->getUser()->getId());
+
+        // Create a form
+        $passwordtab = array(
+            "password" => '',
+            "new_password" => ''
+        );
+        $form_password = $this->createFormBuilder($passwordtab)
+            ->add('password', PasswordType::class, ['label' => 'Votre ancien mot de passe'])
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entre votre nouveau mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options' => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Vérifiez votre mot de passe']
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Changer le mot de passe', 'attr' => ['class' => 'btn btn-brand btn-bold']])
+            ->add('reset', ResetType::class, ['label' => 'Annuler', 'attr' => ['class' => 'btn btn-secondary']])
+            ->getForm();
+
+        $form_password->handleRequest($request);
+
+        if ($form_password->isSubmitted() && $form_password->isValid()) {
+            $old_password = $form_password->get('password')->getData();
+            $credentials['password'] = $old_password;
+            $user = $this->entityManager->getRepository(User::class)->find($this->getUser()->getId());
+
+            if($authenticator->checkCredentials($credentials, $user)) {
+              $new_password = $form_password->get('plainPassword')->getData();
+              $user->setPassword(
+                  $passwordEncoder->encodePassword(
+                      $user,
+                      $new_password
+                  )
+              );
+            }
+
+        }
+
+        $data['form'] = $form_password->createView();
+
+        return $this->render('app/password-settings.html.twig', $data );
+    }
+
 
 
 }
